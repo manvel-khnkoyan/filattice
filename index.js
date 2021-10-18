80556299c58472b994262e3480dfa47b53fed389
Manvel Khnkoyan <manvel.khnkoyan@gmail.com>
2021-10-18 20:12:28 +0400

    Optimized Code, Adde some opportunities

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 /**
- * @param {number} nodes count
- * @return {void}
- */
+* @param {number} nodes count
+* @return {void}
+*/
 class Filattice {
   /**
-   * @param {number} nodes count
-   * @return {void}
-   */
+  * @param {number} nodes count
+  * @return {void} */
   constructor(nodes) {
     this.goldenAngle = 180 * (3 - Math.sqrt(5));
     this.nodes = nodes;
@@ -14,25 +13,22 @@ class Filattice {
   }
 
   /**
-   * @param {number} angle
-   * @return {number}
-   */
+  * @param {number} angle
+  * @return {number} */
   toRadians(angle) {
     return angle * (Math.PI / 180);
   }
 
   /**
-   * @param {number} latitude
-   * @return {number}
-   */
+  * @param {number} latitude
+  * @return {number} */
   latIndex(latitude) {
     return Math.round((90 - latitude)/this.piece);
   }
 
   /**
-   * @param {number} index
-   * @return {Array}
-   */
+  * @param {number} index
+  * @return {Array} */
   nthPoint(index) {
     const theta = (index * this.goldenAngle) % 360;
     const lng = theta > 180 ? theta - 360 : theta;
@@ -41,10 +37,9 @@ class Filattice {
   }
 
   /**
-   * @param {Array} pointOne
-   * @param {Array} pointTwo
-   * @return {number}
-   */
+  * @param {Array} pointOne
+  * @param {Array} pointTwo
+  * @return {number} */
   distanceOf([lat1, lng1], [lat2, lng2]) {
     const dLat = this.toRadians(lat2 - lat1);
     const dlng = this.toRadians(lng2 - lng1);
@@ -59,16 +54,57 @@ class Filattice {
   }
 
   /**
-   * Actually it get nearest
-   * points at first - sorted
-   * @param {Array} point
-   * @param {number} length
-   * @return {Array}
-   */
-  getPoints([lat, lng], length = 1) {
+  * Get Points within given distance
+  * @param {Array} point
+  * @param {number} distance
+  * @return {Array} */
+  pointsInRange([lat, lng], distance) {
+    /* */
+    const points = [];
+    /*
+    * Set points and start point */
+    const startIndex = this.latIndex(lat);
+    const startPoint = this.nthPoint(startIndex);
+    if (this.distanceOf(startPoint, [lat, lng]) < distance) {
+      points.push(startPoint);
+    }
+
+    /*
+    * Loop To Find */
+    let n = 1;
+    while (true) {
+      if (!((indexes) => {
+        for (const i of indexes) {
+          if (i < 0 || i > this.nodes) break;
+          const [iLat, iLng] = this.nthPoint(i);
+          const dist = this.distanceOf([iLat, iLng], [lat, lng]);
+          const max = this.distanceOf([iLat, lng], [lat, lng]);
+          if (dist < distance) {
+            points.push([iLat, iLng, dist]);
+          }
+          if (max > distance) {
+            return false;
+          }
+        }
+        return true;
+      })([startIndex+n, startIndex-n])) {
+        break;
+      };
+      n++;
+    }
+
+    return points;
+  }
+
+  /**
+  * Get Closest Points, sorted by distance
+  * @param {Array} point
+  * @param {number} length
+  * @return {Array} */
+  nearestPoints([lat, lng], length = 1) {
     /*
      * Set points and start point */
-    const index = this.latIndex(lat);
+    const startIndex = this.latIndex(lat);
     const points = Array.from({length});
 
     /*
@@ -85,12 +121,12 @@ class Filattice {
 
     /*
     * Set initial point */
-    const start = this.nthPoint(index);
+    const start = this.nthPoint(startIndex);
     set(points, [...start, this.distanceOf(
         start, [lat, lng])]);
 
     /*
-     * Loop To Find */
+    * Loop To Find */
     let n = 1;
     while (true) {
       if (!((indexes) => {
@@ -110,22 +146,33 @@ class Filattice {
           }
         }
         return true;
-      })([index+n, index-n])) {
+      })([startIndex+n, startIndex-n])) {
         break;
       };
       n++;
     }
 
-    return points.map(
-        ([a, b]) => [a, b],
-    );
+    return points;
   }
 
   /**
-   * @param {Array<string>} point
-   * @return {boolean}
-   */
+   * Get nearest most optimal points
+   * (from 3 to 5)
+   * @param {Array} point
+   * @return {Array} */
+  nearbyPoints(point) {
+    /* */
+    const points = this.nearestPoints(point, 9);
+    const max = points.reduce((a, b) => a + b[2], 0) / 9;
+    return points.filter((p) => p[2] < max);
+  }
+
+  /**
+  * @param {Array<string>} point
+  * @return {boolean} */
   verifyPoint([lat, lng]) {
+    if (lat > 90 || lat < -90) return false;
+    if (lat > 180 || lat < -180) return false;
     const [curlat, curlng] = this.nthPoint(
         this.latIndex(lat),
     );
--- test.js
+++ test.js
@@ -1,37 +1,49 @@
 
-
 const Filattice = require('./index');
-const lattice = new Filattice(1000);
+const lattice = new Filattice(1000000000);
 
-console.log('Get Point Of Yerevan');
-lattice.getPoints(
-    [40.30488967895508, 44.599140000000006], 5,
-).forEach(([lng, lat]) => {
-  console.log(`${lng},${lat},#333,circle,"X"`);
-});
+/*
+* */
+const paint = ([lng, lat], i) => {
+  console.log(`${lng},${lat},#333,circle,"${i+1}"`);
+};
 
-console.log('Get Point Of Antarctida');
-lattice.getPoints(
-    [-69.67029959999999, -69.33574867248535], 5,
-).forEach(([lng, lat]) => {
-  console.log(`${lng},${lat},#333,circle,"X"`);
-});
+/* Example 1
+* */
+console.log('Get Nearby Point');
+lattice.nearbyPoints(
+    [40.3131292, 44.5970966],
+).forEach(paint);
 
-console.log('Get Closest');
-lattice.getPoints(
-    [40.3012902, 44.597541], 1,
-).forEach(([lng, lat]) => {
-  console.log(`${lng},${lat},#333,circle,"X"`);
-});
 
+/* Example 2
+* */
+console.log('Get Nearset 16 Point');
+lattice.nearestPoints(
+    [-53.2968852, -68.4579599], 16,
+).forEach(paint);
+
+
+/* Example 3
+* */
+console.log('Get Points In Range');
+lattice.pointsInRange(
+    [40.1756897, 44.51510469], 0.0005,
+).forEach(paint);
+
+
+/* Example 4
+* */
 console.log('Verifying Points');
-
 console.log(lattice.verifyPoint(
-    ['40.30488967895508', '44.599140000000006'],
+    [40.18874003, 44.52654266357422],
 ));
 console.log(lattice.verifyPoint(
-    ['40.3056684', '44.598411083221436'],
+    ['40.18874003', '44.52654266357422'],
 ));
 console.log(lattice.verifyPoint(
-    [40.3056684, 44.598411083221436],
+    ['40.18874004', '44.52654266357422'],
+));
+console.log(lattice.verifyPoint(
+    [40.18874004, 44.52654266357422],
 ));
